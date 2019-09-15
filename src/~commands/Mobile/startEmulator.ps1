0809af68e19bee7d07cfdc1378b8e776d272a331
astepanian1 <alexsoftware@live.com>
2019-09-15 10:17:10 -0700

    added Mobile powershel helpers

---
+++ src/~commands/Mobile/startEmulator.ps1
@@ -0,0 +1,11 @@
+
+#CTRL+G Start CodeRunner CTRL+ALT+G Stop CodeRunner
+function Start-Emulator { 
+
+$CMD = 'C:\Android\android-sdk\emulator\emulator.exe'
+$arg1 = '-avd'
+$arg2 ='Pixel_3_XL_API_29'
+    & $CMD $arg1 $arg2
+}
+
+&Start-Emulator
