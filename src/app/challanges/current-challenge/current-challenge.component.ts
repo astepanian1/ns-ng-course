79ae1948947b7272428b16c9f3536b51c4b2dc83
astepanian1 <alexsoftware@live.com>
2019-09-21 16:03:55 -0700

    Listview

--- src/app/challanges/current-challenge/current-challenge.component.ts
+++ src/app/challanges/current-challenge/current-challenge.component.ts
@@ -1,13 +1,18 @@
 import { Component, Input } from "@angular/core";
-
+import { ItemEventData } from "tns-core-modules/ui/list-view";
 @Component({
-    selector:'ns-current-challenge',
-    templateUrl:'./current-challenge.component.html',
-    styleUrls:['./current-challenge.component.css'],
-    moduleId:module.id
+    selector: 'ns-current-challenge',
+    templateUrl: './current-challenge.component.html',
+    styleUrls: ['./current-challenge.component.css'],
+    moduleId: module.id
 })
-export class CurrentChallengeComponent{
+export class CurrentChallengeComponent {
     @Input()
-    challenges:string[] =[];
+    challenges: string[] = [];
 
+
+
+    onItemTap(args: ItemEventData) {
+        console.log(args);
+    }
 }
