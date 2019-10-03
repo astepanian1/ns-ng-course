7e4570dd28e355b43e97dd22b39da72cb5f2b4cd
astepanian1 <alexsoftware@live.com>
2019-10-02 21:53:59 -0700

    added icons

--- src/app/shared/ui/action-bar/action-bar.component.ts
+++ src/app/shared/ui/action-bar/action-bar.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { isAndroid } from  "tns-core-modules/platform";
+import { isAndroid } from "tns-core-modules/platform";
 import { Page } from "tns-core-modules/ui/page/page";
 import { RouterExtensions } from 'nativescript-angular/router';
 import { UIService } from '../ui.service';
 
-declare var android:any;
+declare var android: any;
 
 @Component({
   selector: 'ns-action-bar',
@@ -12,46 +12,57 @@ declare var android:any;
   styleUrls: ['./action-bar.component.css']
 })
 export class ActionBarComponent implements OnInit {
-@Input()
-title:string ="";
-@Input() 
-showBackButton = true;
-@Input()
-hasMenu = true;
+  @Input()
+  title: string = "";
+  @Input()
+  showBackButton = true;
+  @Input()
+  hasMenu = true;
 
 
-  constructor(private page:Page,private router:RouterExtensions,private uiService:UIService) { }
+  constructor(private page: Page, private router: RouterExtensions, private uiService: UIService) { }
 
   ngOnInit() {
   }
 
-  
-  get canGoBack(){
+
+  get canGoBack() {
     return this.router.canGoBack() && this.showBackButton;
   }
 
-  get android(){
+  get android() {
     return isAndroid;
   }
 
-  onGoBack(){
+  onGoBack() {
     this.router.backToPreviousPage();
   }
 
-  onToggleMenu(){
+  onToggleMenu() {
     this.uiService.toggleDrawer();
   }
 
-  onLoadedActionBar(){
-    if(isAndroid){
-        const androidNativeToolBar =  this.page.actionBar.nativeView;
-        const backButton = androidNativeToolBar.getNavigationIcon();
-        if(backButton)
-        {
-            backButton.setColorFilter(android.graphics.Color.parseColor("#171717"),android.graphics.PorterDuff.Mode.SRC_ATOP);
-        }
+  onLoadedActionBar() {
+    if (isAndroid) {
+      const androidNativeToolBar = this.page.actionBar.nativeView;
+      //Currently used both for Back button and Menu Bars hense conditional statement.
+      const navigationIcon = androidNativeToolBar.getNavigationIcon();
+      let color = "";
+      
+      //If its a Menu
+      if (this.hasMenu) {
+        color = '#ffffff';
+      }
+      //If its a BackButton
+      else if(this.canGoBack){
+        color = "#171717";
+      }
+
+      if (navigationIcon) {
+        navigationIcon.setColorFilter(android.graphics.Color.parseColor(color), android.graphics.PorterDuff.Mode.SRC_ATOP);
+      }
 
     }
-}
+  }
 
 }
